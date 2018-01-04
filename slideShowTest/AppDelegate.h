3f6c00b5d2038058d4327f7f077c404cac019094
yyf <912907352@qq.com>
2018-01-04 16:53:14 +0800

    Initial Commit

---
+++ slideShowTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  slideShowTest
+//
+//  Created by 叶子 on 2018/1/4.
+//  Copyright © 2018年 叶义峰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
